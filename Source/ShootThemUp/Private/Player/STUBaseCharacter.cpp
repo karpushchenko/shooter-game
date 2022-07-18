e216a7940e685adad62a4a3e5099749ade8f0b51
Ivan Karpushchenko <theblackkarps@gmail.com>
2022-07-18 09:20:44 +0300

    Added spectator mode

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -8,6 +8,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Components/STUHealthComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "GameFramework/Controller.h"
 
 DEFINE_LOG_CATEGORY_STATIC(BaseCharacterLog, All, All);
 
@@ -115,10 +116,12 @@ void ASTUBaseCharacter::OnDeath()
     UE_LOG(BaseCharacterLog, Display, TEXT("Player %s is Dead"), *GetName());
 
     PlayAnimMontage(DeathAnimMontage);
-
     GetCharacterMovement()->DisableMovement();
-
     SetLifeSpan(5.0f);
+    if (Controller)
+    {
+        Controller->ChangeState(NAME_Spectating);
+    }
 }
 
 void ASTUBaseCharacter::OnHealthChanged(float Health)
